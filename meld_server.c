b33ab83d8003c194cf9d3adf83ab07ed9384c047
Rupesh Mehta <rupesh@ubuntu.(none)>
2013-06-04 10:30:37 -0400

    adding force_read() , making code ready for merge with bbsim

--- meld_server.c
+++ meld_server.c
@@ -1,4 +1,4 @@
-/* 
+ /* 
  *  A server using select()
  *  by Martin Broadhurst (www.martinbroadhurst.com)
  */
@@ -18,40 +18,41 @@ static int port=5000;  /* Port to listen on */
 
 void fork_clients(int num){
 
-    char* port="5000";
-    char countbuf[MAXLINE];
-    char* path[3];
-    path[0]=port;
-    path[2]=NULL;
-
-    int i=0;
-    for(i=0; i<num; i++){
-        sprintf(countbuf,"%d",i);
-        path[1]=countbuf;
-        if(fork()==0){
-            execve("blocks",path,NULL);
-        }
-        printf("%d",i);
-    } 
-    printf("Forked clients...\n");
+	char* port="5000";
+	char countbuf[MAXLINE];
+	char* path[3];
+	path[0]=port;
+	path[2]=NULL;
+	
+	int i=0;
+	for(i=0; i<num; i++){
+		sprintf(countbuf,"%d",i);
+		path[1]=countbuf;
+		if(fork()==0){
+		execve("blocks",path,NULL);
+		}
+	printf("%d",i);
+	} 
+printf("Forked clients...\n");
 }
 
 
 void handle(int newsock, fd_set *set)
 {
-    rio_t rio_read;
+	rio_t rio_read;
     /* send(), recv(), close() */
     /* Call FD_CLR(newsock, set) on disconnection */
-    //	int newsock1=*newsock;
-    char buf[MAXLINE];
-    int bytecount;
+	char buf[MAXLINE];
+	int bytecount;
 
-    Rio_readinitb(&rio_read, newsock);
-    bytecount=Rio_readlineb(&rio_read,buf,MAXLINE);
-    printf("Server:%s\n",buf);
+	Rio_readinitb(&rio_read, newsock);
+	bytecount=Rio_readlineb(&rio_read,buf,MAXLINE);
+	printf("Server:%s\n",buf);
 
-    sprintf(buf, "Ack.\n");
-    Rio_writen(newsock,buf,strlen(buf)+1);
+	sprintf(buf, "Ack.\n");
+	Rio_writen(newsock,buf,strlen(buf)+1);
+	FD_CLR(newsock,set);
+	Close(newsock);
 }
 
 int main(void)
@@ -62,13 +63,13 @@ int main(void)
     int maxsock;
     int reuseaddr = 1; /* True */
     struct addrinfo hints, *res;
-    int i=0;
+	int i=0;
     /* Get the address info */
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
 #if 0
-    if (getaddrinfo(NULL, PORT, &hints, &res) != 0) {
+if (getaddrinfo(NULL, PORT, &hints, &res) != 0) {
         perror("getaddrinfo");
         return 1;
     }
@@ -101,18 +102,18 @@ int main(void)
     }
 #endif
 
-    sock=Open_listenfd(port);
+sock=Open_listenfd(port);
 
-    printf("Listening on port:%d\n",port);
+	printf("Listening on port:%d\n",port);
     /* Set up the fd_set */
     FD_ZERO(&socks);
     FD_SET(sock, &socks);
     maxsock = sock;
+	
+//	fork_clients(2);	
 
-    fork_clients(5);	
-    int switch1=1;
 
-    /* Main loop */
+	/* Main loop */
     while (1) {
         unsigned int s;
         readsocks = socks;
@@ -120,45 +121,36 @@ int main(void)
             perror("select");
             return 1;
         }
-        //	printf("Finished Select, s=%d\n",maxsock);
         for (s = 0; s <= maxsock; s++) {
-            //			printf("Inside the for loop.\n");
+			printf("Inside the for loop.\n");
             if (FD_ISSET(s, &readsocks)) {
-                //   printf("socket %d was ready\n", s);
+                printf("socket %d was ready\n", s);
                 if (s == sock) {
                     /* New connection */
                     int newsock;
                     struct sockaddr_in their_addr;
                     unsigned int size = sizeof(struct sockaddr_in);
                     newsock = accept(sock, (struct sockaddr*)&their_addr, &size);
-                    printf("Accepted a connection.\n");
-                    if (newsock == -1) {
+                   	printf("Accepted a connection.\n");
+				   	if (newsock == -1) {
                         perror("accept");
                     }
                     else {
                         printf("Got a connection from %s on port %d\n", inet_ntoa(their_addr.sin_addr), htons(their_addr.sin_port));
-                        FD_SET(newsock, &socks);
+		/*	if(fork()==0){
+			handle(newsock,&socks);
+			}*/
+				                        
+			FD_SET(newsock, &socks);
                         if (newsock > maxsock) {
                             maxsock = newsock;
                         }
-
-                        if(fork()==0){
-                            handle(newsock,&socks);
-                            exit(0);
-                        }
-                        FD_CLR(newsock,&socks);
-                        printf("closing the connection");
-                        Close(newsock);
-                    }          
-
+                    }
                 }
 
             }
         }
-        //	if(switch1){
-        //		fork_clients(2);
-        //		switch1=0;
-        //	}
+
     }
 
     close(sock);
